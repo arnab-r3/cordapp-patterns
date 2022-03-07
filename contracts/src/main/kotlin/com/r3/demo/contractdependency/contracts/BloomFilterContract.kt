41f9c8f974ab85349a9774cebde3270d8d4fa51d
Arnab Chatterjee <arnab.chatterjee@r3.com>
2022-03-07 13:07:01 +0530

    Add contract bloom filter checks in contracts
    Signed-off-by: Arnab Chatterjee <arnab.chatterjee@r3.com>

--- contracts/src/main/kotlin/com/r3/demo/contractdependency/contracts/BloomFilterContract.kt
+++ contracts/src/main/kotlin/com/r3/demo/contractdependency/contracts/BloomFilterContract.kt
@@ -1,12 +1,15 @@
 package com.r3.demo.contractdependency.contracts
 
+import com.r3.demo.contractdependency.states.BloomFilterState
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.transactions.LedgerTransaction
 
 class BloomFilterContract: Contract {
     override fun verify(tx: LedgerTransaction) {
-
+        val outputsOfType = tx.outputsOfType<BloomFilterState>()
+        val checkContains = outputsOfType.single().checkContains("test")
+        println("checkContains: $checkContains")
     }
 
     interface BloomFilterCommands : CommandData {
